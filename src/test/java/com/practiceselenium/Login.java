1e8c03b5427e55cd8f5ff905d9d6a9d7aa5b9ea5
Mesbah26 <mesbahulislam26@gmail.com>
2025-09-27 12:51:01 +0100

    First commit

---
+++ src/test/java/com/practiceselenium/Login.java
@@ -0,0 +1,29 @@
+
+package com.practiceselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+	
+	 void getLogin() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		// driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+		
+	}
+	public static void main(String[] args) {
+		Login obj=new Login();
+		obj.getLogin();
+		
+	}
+}
+	
+	
+	      
+
+
